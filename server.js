23a7471b6336f5a8532bb20dd095f59cfb554926
Kam <seekayel@users.noreply.github.com>
2022-01-31 11:52:42 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
+    console.log(JSON.stringify(process.env,null,2))
     res.send(`Yo! => ${new Date().toISOString()}`)
 })
 app.listen(process.env.PORT || 3000)
