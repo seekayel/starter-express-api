a8f6a6fa445aedc5a0cc7dd0f45321150523e430
Kam Lasater <ckl@seekayel.com>
2022-10-20 12:01:49 -0400

    Added ls action

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const express = require('express')
+const fs = require('fs')
+
 const app = express()
+
+app.get('/ls', (req,res) => {
+  let ls = fs.readdirSync('.')
+  res.json(ls)
+})
+
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     console.log(JSON.stringify(process.env,null,2))
